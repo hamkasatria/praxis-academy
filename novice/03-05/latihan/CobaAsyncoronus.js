20294a368e6969573322f34e6b0015c164a6a759
hamkasatria <muhhamkasatrianto@gmail.com>
2020-09-11 09:57:59 +0700

    mencba acyncoronus

---
+++ novice/03-05/latihan/CobaAsyncoronus.js
@@ -0,0 +1,14 @@
+
+
+
+
+//merupa pemrograman syncoronus
+console.log(4);
+a=function(){console.log(5)}();
+console.log(6);
+
+
+//merupakan program asyncoronus dimana akan delay 3 detik dahulu
+console.log("1");
+setTimeout(()=>{console.log("2")},3000);
+console.log("3");
